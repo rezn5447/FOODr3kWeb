f183812b19ed8973adfc9f347859e43f29c1bd4e
Resdan Logan <rezn5447@gmail.com>
2017-04-12 10:53:30 -0700

    switching this to reactjs animation basics

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
 import React, { Component } from 'react';
 import {logo} from './logo.svg';
-import Map from './components/Map';
 import './App.css';
+
+const styles = {
+    transition: 'all 1s ease-out'
+};
 
 class App extends Component {
 
   componentWillMount() {
-
+      this.state = {};
   }
+  onHide() {
+      this.setState({
+          opacity: 0
+      });
+  }
+
+  onScale() {
+      this.setState({
+          scale: this.state.scale > 1 ? 1 : 1.3
+      });
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          <div>
-            <Map />
+      <div>
+          <nav>
+              <div className="nav-wrapper orange darken-2">
+                  <ul className="left">
+                      <li className="active"><a href="#">TRANSITIONS</a></li>
+                      <li><a href="#">ANIMATIONS</a></li>
+                      <li><a href="#">REACTJS + CSS TRANSITIONS</a></li>
+                      <li><a href="#">REACTJS + CSS ANIMATIONS</a></li>
+                  </ul>
+              </div>
+          </nav>
+          <div className="container">
+              <div className="row">
+                  <div className="s12">
+
+                  </div>
+              </div>
+
+              <div className="row">
+                  <div className="s8 offset-s2 center-align">
+                      <div className="card deep-purple z-depth-2"
+                           style={{...styles, opacity: this.state.opacity, transform: 'scale(' + this.state.scale + ')'}}>
+                          <div className="card-content white-text">
+                              <span className="card-title">Awesome Animations!</span>
+                              <p>CSS Animations are pretty cool. But combined with ReactJS ... &lt;3</p>
+                          </div>
+                          <div className="card-action">
+                              <a onClick={this.onHide.bind(this)} style={{cursor: 'pointer'}}>HIDE</a>
+                              <a onClick={this.onScale.bind(this)} style={{cursor: 'pointer'}}>SCALE</a>
+                          </div>
+                      </div>
+                  </div>
+              </div>
           </div>
-        </p>
       </div>
+
     );
   }
 }
